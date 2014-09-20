60122e03375d908ff927f31150864c5f33593650
mike stedman <github@ravuya.com>
2014-09-19 21:45:58 -0600

    holy shit it works

--- get-addresses.py
+++ get-addresses.py
@@ -1,4 +1,17 @@
 import xml.etree.ElementTree as et
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+def get_address_from_item(itemguid):
+  # load page
+  response = urllib2.urlopen(itemguid)
+  content = response.read()
+  response.close()
+
+  soup = BeautifulSoup(content)
+  # so boring of me
+  return soup.find(id='MapLink').parent.contents[0].string
+
 
 tree = et.parse('trucks.rss') # TODO
 root = tree.getroot()
@@ -18,4 +31,4 @@ root = tree.getroot()
 for car in root.find('channel').findall('item'):
   title = car.find('title').text
   guid = car.find('guid').text
-  print title, guid
+  print title, guid, get_address_from_item(guid)
