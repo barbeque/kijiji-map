f5dbe559b00906c4e6bade9a8f9a16e73e35c417
mike <github@ravuya.com>
2015-05-21 16:27:52 -0600

    Geolocate actually works now

--- geolocate.py
+++ geolocate.py
@@ -6,12 +6,30 @@ API_KEY_PATH = "geolocation_api_key"
 
 GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
+class GeolocateFailure(Exception):
+    pass
+
 def geolocate(config, address):
     # convert address to lat-longs
+
+    # fetch from google api
     api_key = config[API_KEY_PATH]
     payload = { "address": address, "key": api_key }
     result = requests.get(GOOGLE_MAPS_URL, params = payload)
-    print result.json()
+
+    # interpret results
+    result_object = result.json()
+    result_results = result_object["results"]
+    if len(result_results) < 1:
+        raise GeolocateFailure("Could not find a single result for address '{}'" % address)
+
+    # grab first geometry result to extract lat-long
+    result_geometry = result_results[0]["geometry"]
+    latlong = result_geometry["location"]
+
+    # This is separated to try and insulate the downstream code
+    # from any changes Google might make.
+    return { "lat": latlong['lat'], "long": latlong['lng'] }
     
 def configure(secretsFilePath):
     # load from config
@@ -21,4 +39,5 @@ def configure(secretsFilePath):
         
 if __name__ == '__main__':
     config = configure('secrets.yml')
-    geolocate(config, '1600 pennsylvania avenue washington dc')
+    ll = geolocate(config, '1600 pennsylvania avenue washington dc')
+    print ll
